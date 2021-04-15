2c02bcfb3c3a81e64bf5183e877fcae5e22610f8
hungtran97 <hungtran.nh97@gmail.com>
2021-04-15 16:27:11 +0700

    add i18n

--- src/modules/login/login.tsx
+++ src/modules/login/login.tsx
@@ -1,43 +1,47 @@
-import React, { FormEvent } from "react";
-import { observer } from "mobx-react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import "./login.css";
-import userStore from "stores/user/userStore";
-import sessionStore from "stores/session/sessionStore";
-import { logger } from "core/logger";
-import { Redirect } from "react-router-dom";
+import React, { FormEvent } from 'react'
+import { observer } from 'mobx-react'
+import { Button, Col, Container, Form, Row } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
+import './login.css'
+import userStore from 'stores/user/userStore'
+import sessionStore from 'stores/session/sessionStore'
+import { logger } from 'core/logger'
+import { Redirect } from 'react-router-dom'
 
 const Login = observer(() => {
+  const { t, i18n } = useTranslation('translation')
   if (sessionStore.isLoggedIn) {
     return (
       <Redirect
         to={{
-          pathname: "/home",
+          pathname: '/home',
         }}
       />
-    );
+    )
   }
 
-  const user = userStore.newEmptyUser();
+  const user = userStore.newEmptyUser()
 
   const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
-    const res = await userStore.login();
-    logger.debug("res:", res);
-    logger.debug("isLoggedIn:", sessionStore.isLoggedIn);
-  };
+    event.preventDefault()
+    const res = await userStore.login()
+    logger.debug('res:', res)
+    logger.debug('isLoggedIn:', sessionStore.isLoggedIn)
+  }
 
   return (
     <Container>
+      <Button onClick={() => i18n.changeLanguage('vi')}> VI </Button>
+      <Button onClick={() => i18n.changeLanguage('en')}> EN </Button>
       <Row className="justify-content-md-center">
         <Col sm={6}>
           <Form onSubmit={handleSubmit} className="login">
             <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
+              <Form.Label>{t('login.email.label')}</Form.Label>
               <Form.Control
                 type="email"
                 name="email"
-                placeholder="Enter email"
+                placeholder={t('login.email.placeholder')}
                 // value={user.email}
                 onChange={(event) => (user.email = event.target.value)}
               />
@@ -47,24 +51,24 @@ const Login = observer(() => {
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
+              <Form.Label>{t('login.password.label')}</Form.Label>
               <Form.Control
                 type="password"
                 name="password"
-                placeholder="Password"
+                placeholder={t('login.password.placeholder')}
                 // value={user.password}
                 onChange={(event) => (user.password = event.target.value)}
               />
             </Form.Group>
 
             <Button variant="primary" type="submit">
-              Submit
+              {t('common.button.submit')}
             </Button>
           </Form>
         </Col>
       </Row>
     </Container>
-  );
-});
+  )
+})
 
-export default Login;
+export default Login
